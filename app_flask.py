6d8d6972256a1a50c92d639cfeb721b714876ada
yan kaled <yanchoumansimionato@gmail.com>
2025-11-28 14:49:25 -0300

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -7,7 +7,37 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+@app.route('/soma')
+def soma():
+    v1 = float(request.args.get('valor1',0))
+    v2 = float(request.args.get('valor2',0))
+    return {'resultado': v1 + v2}
+
+@app.route('/subtrair')
+def subtrair():
+    v1 = float(request.args.get('valor1',0))
+    v2 = float(request.args.get('valor2',0))
+    return {'resultado': v1 - v2}
+
+@app.route('/multiplicar')
+def multiplicar():
+    v1 = float(request.args.get('valor1',0))
+    v2 = float(request.args.get('valor2',0))
+    return {'resultado': v1 * v2}
+
+@app.route('/dividir')
+def dividir():
+    v1 = float(request.args.get('valor1',0))
+    v2 = float(request.args.get('valor2',1))
+    if v2 == 0:
+        return {'Erro':'frase'}
+    return {'resultado': v1 / v2}
+
+
+
+    
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
